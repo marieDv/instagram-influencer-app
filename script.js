0ed44a0fcb21b0ebc070dbda592d603097735099
marie <dvorzak.marie@gmx.at>
2020-07-20 13:07:47 +0200

    update random

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // window.location.replace("http://stackoverflow.com");
 window.location.replace("https://fhwn.qualtrics.com/jfe/form/SV_eQEUQ2Z4RihSgOF");
+let randomNumber = Math.round((Math.random() * 1) + 1);
+console.log(randomNumber);
+if(randomNumber === 1){
+  window.location.replace("https://fhwn.qualtrics.com/jfe/form/SV_eQEUQ2Z4RihSgOF");
+}else{
+  window.location.replace("http://stackoverflow.com");
+}
