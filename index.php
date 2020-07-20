1f78d7dbf464e3b74c479c49ff8fa92d35b46f73
marie <dvorzak.marie@gmx.at>
2020-07-20 12:59:32 +0200

    update

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-echo ‘<?php include_once(“home.html”); ?>’
+<!doctype html>
+<html lang="en">
+<html>
+<meta charset="utf-8">
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello Verena</p>'; ?> 
+ </body>
+</html>
