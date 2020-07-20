cc72d5e4d50aa76c3ce89be669f4d8c30c126138
marie <dvorzak.marie@gmx.at>
2020-07-20 13:11:43 +0200

    update location replace

--- index.php
+++ index.php
@@ -3,11 +3,9 @@
 <html>
 <meta charset="utf-8">
  <head>
-  <title>PHP Test</title>
+  <title>redirect to survey</title>
  </head>
  <body>
- <?php echo '<p>HEy VErENa! :D</p>'; ?> 
  <script src="script.js"></script>
-
  </body>
 </html>
