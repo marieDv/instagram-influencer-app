724dc96c3446f52067adb8fa2c2084cd28a09dd6
marie <dvorzak.marie@gmx.at>
2020-08-20 11:29:17 +0200

    links update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let randomNumber = Math.round((Math.random() * 1) + 1);
 if(randomNumber === 1){
-  window.location.replace("https://fhwn.qualtrics.com/jfe/form/SV_eQEUQ2Z4RihSgOF");
+  window.location.replace("https://fhwn.qualtrics.com/jfe/form/SV_b7n2o2WXLwdXodT");
 }else{
-  window.location.replace("http://stackoverflow.com");
+  window.location.replace("https://fhwn.qualtrics.com/jfe/form/SV_dg4s5immRoOi3Bz");
 }
